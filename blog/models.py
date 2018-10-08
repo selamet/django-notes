821c3d338aa457098ecad5e097e298bca28b6120
Selamet Şamlı <selametsamli@Selamet-MacBook-Pro.local>
2018-10-08 21:32:44 +0300

    created models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length = 100 ,blank = True, null =True, verbose_name='Başlık ',
+                             help_text = 'Başlık bilgisi burada girilir.')
+    content = models.TextField(max_length=1000, verbose_name='İçerik', null = True , blank = False)
+
+    created_date = models.DateField(auto_now_add=True, auto_now=False)
+
+    class Meta:
+        verbose_name = 'Gönderi'
+        verbose_name_plural = 'Gönderiler'
+        ordering = ['id']
+
+
+    def __str__(self):
+        return '%s' % (self.title)
