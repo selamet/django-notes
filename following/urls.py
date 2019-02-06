53753cb8bac1ae70e4a648ad404209843231c4d8
selametsamli <selamet96@gmail.com>
2019-02-06 17:50:30 +0300

    modal page edited, follow operations

--- following/urls.py
+++ following/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from .views import kullanici_takip_et_cikar, followed_or_followers_list
+from .views import kullanici_takip_et_cikar, followed_or_followers_list, kullanici_modal_takip_et_cikar
 
 urlpatterns = [
 
     url(r'^takiplesme-sistemi/$', kullanici_takip_et_cikar, name='kullanici-takip-et-cikar'),
     url(r'^followed-or-followers-list/(?P<follow_type>[-\w]+)/$', view=followed_or_followers_list,
         name='followed-or-followers-list'),
-
+    url(r'^modal-takip-et-cikar/$', kullanici_modal_takip_et_cikar, name='modal-takip-et-cikar')
 ]
--- following/views.py
+++ following/views.py
@@ -8,11 +8,53 @@ from django.template.loader import render_to_string
 from django.contrib.auth.models import User
 
 
+def kullanici_modal_takip_et_cikar(request):
+    response = sub_kullanici_takip_et_cikar(request)
+    follow_type = request.GET.get('follow_type')
+    data = response.get('data')
+    owner = request.GET.get('owner')
+    followed = response.get('followed')
+
+    if owner == request.user.username:
+        my_followed = Following.get_followed_username(user=request.user)
+        takipci_ve_takip_edilen_sayisi = Following.kullanici_takip_edilenler_ve_takipciler(request.user)
+        context = {'user': followed, 'takipciler': takipci_ve_takip_edilen_sayisi['takipciler'],
+                   'takip_edilenler': takipci_ve_takip_edilen_sayisi['takip_edilenler']}
+        html_render_takip_durum = render_to_string('auths/profile/include/following/following_partion.html',
+                                                   context=context,
+                                                   request=request)
+        following = []
+        if follow_type == "followed":
+            following = Following.get_followed(user=request.user)
+        elif follow_type == "followers":
+            following = Following.get_followers(user=request.user)
+
+        html = render_to_string('following/profile/include/following_followed_list.html', context={
+            'following': following, 'my_followed': my_followed, 'follow_type': follow_type}, request=request)
+        data.update({'html_takip_render': html_render_takip_durum, 'html': html, 'owner': True})
+    else:
+        data.update({'owner': False})
+    return JsonResponse(data=data)
+
+
 def kullanici_takip_et_cikar(request):
+    response = sub_kullanici_takip_et_cikar(request)
+    data = response.get('data')
+    followed = response.get('followed')
+    takipci_ve_takip_edilen_sayisi = Following.kullanici_takip_edilenler_ve_takipciler(followed)
+    context = {'user': followed, 'takipciler': takipci_ve_takip_edilen_sayisi['takipciler'],
+               'takip_edilenler': takipci_ve_takip_edilen_sayisi['takip_edilenler']}
+    html = render_to_string('auths/profile/include/following/following_partion.html', context=context, request=request)
+    data.update({'html': html})
+
+    return JsonResponse(data=data)
+
+
+def sub_kullanici_takip_et_cikar(request):
     if not request.is_ajax():
         return HttpResponseBadRequest()
 
-    data = {'html': '', 'is_valid': True, 'msg': 'Takipden Çıkar'}
+    data = {'takip_durum': True, 'html': '', 'is_valid': True, 'msg': 'Takipden Çıkar'}
     follower_username = request.GET.get('follower_username', None)
     followed_username = request.GET.get('followed_username', None)
 
@@ -25,15 +67,9 @@ def kullanici_takip_et_cikar(request):
         Following.kullanici_takip_et(follower=follower, followed=followed)
     else:
         Following.kullanici_takipten_cikar(followed=followed, follower=follower)  # takipten çıkarma işlemi
-        data.update({'msg': 'Takip Et'})
+        data.update({'msg': 'Takip Et', 'takip_durumm': False})
 
-    takipci_ve_takip_edilen_sayisi = Following.kullanici_takip_edilenler_ve_takipciler(followed)
-    context = {'user': followed, 'takipciler': takipci_ve_takip_edilen_sayisi['takipciler'],
-               'takip_edilenler': takipci_ve_takip_edilen_sayisi['takip_edilenler']}
-    html = render_to_string('auths/profile/include/following/following_partion.html', context=context, request=request)
-    data.update({'html': html})
-
-    return JsonResponse(data=data)
+    return {'data': data, 'followed': followed}
 
 
 def followed_or_followers_list(request, follow_type):
